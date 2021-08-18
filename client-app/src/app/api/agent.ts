2fdd188e4406990e2714b1f4bce38a40253b6cea
Mahdi Nikbakht <mahdinikbakht7105@gmail.com>
2021-08-18 20:08:30 +0430

    End of setion 8_01

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -12,7 +12,7 @@ axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.response.use(async response => {
     try {
-        await sleep(1000);
+        await sleep(1100);
         return response;
     } catch (error) {
         console.log(error);
