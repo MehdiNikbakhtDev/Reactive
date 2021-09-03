3f06df148b59d187c78347c5f508fecc5e454e5e
Mahdi Nikbakht <mahdinikbakht7105@gmail.com>
2021-09-03 00:45:50 -0700

    Edit end Section 11 -14000611

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Button, Icon, Item, Segment } from 'semantic-ui-react';
+import { Button, Icon, Item, ItemGroup, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
-import {format} from 'date-fns'
+import { format } from 'date-fns'
 interface Props {
     activity: Activity
 }
@@ -10,40 +10,42 @@ interface Props {
 export default function ActivityListItem({ activity }: Props) {
 
     return (
-<Segment.Group>
-    <Segment>
-        <Item>
-            <Item.Image size='tiny' circular src='/assets/user.png'/>
-            <Item.Content>
-                <Item.Header as={Link} to={`/activities/${activity.id}`}>
-                    {activity.title}
-                    </Item.Header>
-                    <Item.Description>Hosted by Bob</Item.Description>
-            </Item.Content>
-        </Item>
+        <Segment.Group>
+            <Segment>
+                <ItemGroup>
+                    <Item>
+                        <Item.Image style={{ marginBottom: 3 }} size='tiny' circular src='/assets/user.png' />
+                        <Item.Content  >
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
+                            <Item.Description>Hosted by Bob</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </ItemGroup>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' />{format(activity.date!, 'dd MMM yyyy h:mm aa')}
+                    <Icon name='marker' />{activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees go here
     </Segment>
-    <Segment>
-        <span>
-            <Icon name='clock'/>{format(activity.date!, 'dd MMM yyyy h:mm aa')}
-            <Icon name='marker'/>{activity.venue}
-        </span>
-    </Segment>
-    <Segment secondary>
-        Attendees go here
-    </Segment>
-    <Segment clearing>
-        <span>
-            {activity.description}
-        </span>
-        <Button 
-        as={Link} 
-        to={`/activities/${activity.id}`}
-        color= 'teal'
-        floated='right'
-        content='View'
-         />
-    </Segment>
-</Segment.Group>
+            <Segment clearing>
+                <span>
+                    {activity.description}
+                </span>
+                <Button
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='teal'
+                    floated='right'
+                    content='View'
+                />
+            </Segment>
+        </Segment.Group>
 
 
         // <Item key={activity.id}>
