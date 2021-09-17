ad84c0dc9a5ac16bce10ea558eb5b05cdaa75ee1
Mahdi Nikbakht <mahdinikbakht7105@gmail.com>
2021-09-17 13:02:33 +0430

    End of section 15

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,3 +1,5 @@
+import { Profile } from "./profile";
+
 export interface Activity {
     id: string;
     title: string;
@@ -6,4 +8,36 @@ export interface Activity {
     category: string;
     city: string;
     venue: string;
+    hostUsername:string;
+    isCancelled: boolean;
+    isGoing:boolean;
+    isHost:boolean;
+    host?:Profile;
+    attendees:Profile[];
+}
+export class Activity implements Activity{
+    constructor(init?:ActivityFormValues){
+        Object.assign(this,init);
+    }
+}
+export class ActivityFormValues{
+    id?:string =undefined;
+    title: string='';
+    category:string='';
+    description: string='';
+    date: Date|null=null
+    city:string = '';
+    venue:string =''
+
+    constructor(activity?:Activity) {
+        if(activity){
+            this.id=activity.id;
+            this.category=activity.category;
+            this.city=activity.city;
+            this.date=activity.date;
+            this.description=activity.description;
+            this.title=activity.title;
+            this.venue=activity.venue;
+        }
+    }
 }
