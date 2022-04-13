b114e5e2c88ebf72e9339fb0851cde96d755beca
MehdiNikbakht <mahdinikbakht7105@gmail.com>
2022-04-13 23:54:41 +0430

    1

--- mehdi.App/Activities/ActivityParams.cs
+++ mehdi.App/Activities/ActivityParams.cs
@@ -5,8 +5,8 @@ namespace mehdi.App.Activities
 {
     public class ActivityParams : PagingParams
     {
-         public bool IsGoing { get; set; } 
-         public bool IsHost { get; set; }
+           public bool IsGoing { get; set; } 
+          public bool IsHost { get; set; }
          public DateTime StartDate { get; set; } = DateTime.UtcNow;
     }
 }
